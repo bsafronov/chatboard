fd6514d226227cd188ec86a24b069ac405627ac0
Bogdan Safronov <safronovba@modeltech.ru>
2026-04-06 22:24:52 +0300

    add table template

--- src/routes/_protected/tables.$tableId/index.tsx
+++ src/routes/_protected/tables.$tableId/index.tsx
@@ -1,6 +1,15 @@
+import { eq, useLiveSuspenseQuery } from "@tanstack/react-db";
 import { createFileRoute, Link } from "@tanstack/react-router";
+import {
+	type ColumnDef,
+	getCoreRowModel,
+	useReactTable,
+} from "@tanstack/react-table";
 import { LucideSettings } from "lucide-react";
+import { useMemo } from "react";
 import { buttonVariants } from "@/components/ui/button";
+import { columnCollection } from "@/features/column/collection";
+import { DataTable } from "@/features/table/data-table";
 
 export const Route = createFileRoute("/_protected/tables/$tableId/")({
 	component: RouteComponent,
@@ -8,6 +17,30 @@ export const Route = createFileRoute("/_protected/tables/$tableId/")({
 
 function RouteComponent() {
 	const { tableId } = Route.useParams();
+
+	const { data: columns } = useLiveSuspenseQuery((q) =>
+		q
+			.from({ column: columnCollection })
+			.where(({ column }) => eq(column.tableId, tableId)),
+	);
+
+	const resolvedColumns = useMemo(() => {
+		return columns.map((column) => {
+			const def: ColumnDef<Record<string, unknown>> = {
+				id: column.id,
+				header: column.name,
+				accessorKey: column.id,
+			};
+			return def;
+		});
+	}, [columns]);
+
+	const table = useReactTable({
+		getCoreRowModel: getCoreRowModel(),
+		data: [],
+		columns: resolvedColumns,
+	});
+
 	return (
 		<div>
 			<Link
@@ -17,6 +50,7 @@ function RouteComponent() {
 			>
 				<LucideSettings />
 			</Link>
+			<DataTable table={table} />
 		</div>
 	);
 }
